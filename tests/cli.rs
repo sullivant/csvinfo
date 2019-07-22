7ccab883cebc6df8e09c738761442d5347e74458
Thomas Sullivan <thomas.l.sullivan@vumc.org>
2019-07-22 15:40:12 -0500

    Added test case for quoted values.

--- tests/cli.rs
+++ tests/cli.rs
@@ -23,3 +23,17 @@ fn simple_file() -> Result<(), Box<std::error::Error>> {
     cmd.assert().success();
     Ok(())
 }
+
+#[test]
+fn quoted_file() -> Result<(), Box<std::error::Error>> {
+    let mut cmd = Command::cargo_bin("csvinfo").unwrap();
+    cmd.arg("tests/data/quoted.csv");
+    cmd.arg("-q");
+    cmd.arg("-s");
+
+    // If quoted, properly, this will contain "City,State" as one of the field headers.
+    let predicate_fn = predicate::str::contains("City,State");
+    cmd.assert().success().stdout(predicate_fn);
+
+    Ok(())
+}
